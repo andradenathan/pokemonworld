e7da8f631d7b973ddeda8756ecdfd911ecf779df
andradenathan <nathan.nhdsk@gmail.com>
2020-10-04 21:15:47 -0300

    + Return Pokemon status and catch the pokemon

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 var pokemon;
 var pokemonInfo;
+var catch_pokemon;
+var pokemon_images;
+var id = 0;
 var x = new Object();
 
 // Creating a request 
 function getAllPokemon(){
-    let apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=1000';
     fetch(apiURL)
     .then(response => response.json())
     .then(function(pokeData){
@@ -19,10 +22,21 @@ function getAllPokemon(){
     .then(function(pokeData){
     console.log(pokeData)
     pokemonInfo = pokeData
+    document.getElementById('paragraph').innerHTML = '<h2>Your Pokemon information below</h2>';
+    document.getElementById("who_is_this_pokemon").src = pokemon_images;
     document.getElementById('pokeName').innerText = pokemonInfo.species.name
     document.getElementById('pokeType').innerText = pokemonInfo.types[0].type.name
+    if(pokemonInfo.types.length > 1){
+        document.getElementById('pokeType2').innerText = pokemonInfo.types[1].type.name
+    } else {
+        document.getElementById('ability2').innerText = 'None';
+    }
     document.getElementById('ability1').innerText = pokemonInfo.abilities[0].ability.name
-    document.getElementById('ability2').innerText = pokemonInfo.abilities[1].ability.name
+    if(pokemonInfo.abilities.length > 1){
+        document.getElementById('ability2').innerText = pokemonInfo.abilities[1].ability.name
+    } else{
+        document.getElementById('ability2').innerText = 'None';
+    }
     document.getElementById('status1').innerText = pokemonInfo.stats[0].base_stat
     document.getElementById('status2').innerText = pokemonInfo.stats[1].base_stat
     document.getElementById('status3').innerText = pokemonInfo.stats[2].base_stat
@@ -30,51 +44,87 @@ function getAllPokemon(){
     document.getElementById('status5').innerText = pokemonInfo.stats[4].base_stat
     document.getElementById('status6').innerText = pokemonInfo.stats[5].base_stat
     document.getElementById('pokeWeight').innerText = pokemonInfo.weight
+    id = pokemonInfo.id;
 })
   }
 
 
-function returnPokemon(){
+ function returnPokemon(){
     var x = document.getElementById("pokemon").value
-    switch(x){
-        case 'bulbasaur':
-            console.log(pokemon.results[0])
-            showOnePokemon(pokemon.results[0].url)
-        break;
-        case 'ivysaur':
-            console.log(pokemon.results[1])
-            showOnePokemon(pokemon.results[1].url)
-        break;
-        case 'venusaur':
-            console.log(pokemon.results[2])
-            showOnePokemon(pokemon.results[2].url)
-        break;
-        case 'charmander':
-            console.log(pokemon.results[3])
-            showOnePokemon(pokemon.results[3].url)
-        break;
-        case 'charmeleon':
-            console.log(pokemon.results[4])
-            showOnePokemon(pokemon.results[4].url)
-        break;
-        case 'charizard':
-            console.log(pokemon.results[5])
-            showOnePokemon(pokemon.results[5].url)
-        break;
-        case 'squirtle':
-            console.log(pokemon.results[6])
-            showOnePokemon(pokemon.results[6].url)
-        break;
-        case 'wartortle':
-            console.log(pokemon.results[7])
-            showOnePokemon(pokemon.results[7].url)
-        break;
-        case 'blastoise':
-            console.log(pokemon.results[8])
-            showOnePokemon(pokemon.results[8].url)
-        break;
+    for(let i = 0; i < pokemon.results.length; i++){
+        if(x == pokemon.results[i].name){
+            getPokemonImage(i + 1);
+            console.log(pokemon.results[i])
+            showOnePokemon(pokemon.results[i].url)
+            catch_pokemon = pokemon.results[i].name  
+        }
     }
 }
+
+function getPokemonImage(pokeId){
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon-form/' + pokeId + '/';
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(function(pokeData){
+    console.log(pokeData)
+    pokemon_images = pokeData.sprites.front_default;
+    })
+}
+
+
+//     var x = document.getElementById("pokemon").value
+//     switch(x){
+//         case 'bulbasaur':
+//             console.log(pokemon.results[0])
+//             showOnePokemon(pokemon.results[0].url)
+//             document.getElementById("who_is_this_pokemon").src = "bulbasaur.png";
+//             catch_pokemon = x;
+//         break;
+//         case 'ivysaur':
+//             console.log(pokemon.results[1])
+//             showOnePokemon(pokemon.results[1].url)
+//             catch_pokemon = x;
+//         break;
+//         case 'venusaur':
+//             console.log(pokemon.results[2])
+//             showOnePokemon(pokemon.results[2].url)
+//             catch_pokemon = x;
+//         break;
+//         case 'charmander':
+//             console.log(pokemon.results[3])
+//             showOnePokemon(pokemon.results[3].url)
+//             catch_pokemon = x;
+//         break;
+//         case 'charmeleon':
+//             console.log(pokemon.results[4])
+//             showOnePokemon(pokemon.results[4].url)
+//             catch_pokemon = x;
+//         break;
+//         case 'charizard':
+//             console.log(pokemon.results[5])
+//             showOnePokemon(pokemon.results[5].url)
+//             catch_pokemon = x;
+//         break;
+//         case 'squirtle':
+//             console.log(pokemon.results[6])
+//             showOnePokemon(pokemon.results[6].url)
+//             catch_pokemon = x;
+//         break;
+//         case 'wartortle':
+//             console.log(pokemon.results[7])
+//             showOnePokemon(pokemon.results[7].url)
+//             catch_pokemon = x;
+//         break;
+//         case 'blastoise':
+//             console.log(pokemon.results[8])
+//             showOnePokemon(pokemon.results[8].url)
+//             catch_pokemon = x;
+//         break;
+//         default:
+//             alert('Sorry, your Pokémon does not exist');
+//     }
+// }
+
 
 function greetNewUser(){
     name = prompt("Enter your name as Pokemon trainer: ");
@@ -87,6 +137,19 @@ window.onload = function(){
     }
 }
 
+function catchPokemon(){
+    var pokevalid = localStorage.getItem('pokemon ' + id);
+        if(catch_pokemon != pokevalid){
+            localStorage.setItem('pokemon ' + id, catch_pokemon)
+            alert('You caught ' + pokemonInfo.species.name + "!")
+        } else{
+            alert('You already caught this Pokemon')
+        }
+}
+
+function showMyPokemon(){
+    window.location.replace('/Users/USUÁRIO/Documents/pokemonworld/pokemonlist.html')
+}
 
 
 getAllPokemon()
--- scripts/script01.js
+++ scripts/script01.js
@@ -1,68 +1,51 @@
-(function(){
-  "use strict";
-  /* Start of your code */
-  function greetMe(yourName) {
-    alert('Hello ' + yourName);
+var pokemon;
+var pokemonInfo;
+var pokemon_images;
+var id = 0;
+var x = new Object();
+
+function returnHome(){
+  window.location.replace('/Users/USUÁRIO/Documents/pokemonworld/index.html')
+}
+
+// Creating a request 
+function getAllPokemon(){
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=100';
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(function(pokeData){
+    console.log(pokeData)
+    for(let j = 0; j < pokeData.results.length; j++){ 
+      for(let i = 0; i < pokeData.results.length; i++){
+            if(localStorage.getItem('pokemon ' + j) == pokeData.results[i].name){
+                showOnePokemon(pokeData.results[i].url);
+                getPokemonImage(i + 1);
+                var newdiv = document.createElement('div')
+                var pokemonImages = document.getElementById("who_is_this_pokemon").src = pokemon_images;
+                var pokemonNames = document.getElementById('newPokeName').innerText = pokeData.results[i].name;
+            }
+        }
+      }
+    })
   }
-  
-  greetMe('World');
-  /* End of your code */
-})();
 
-/*var a;
-console.log('The value of a is ' + a); // It'll return The value of a is undefined because a doesn't have a setted value
-console.log(a + 2) // It'll return NaN because "a" is undefined 
-
-var n = null;
-console.log(n * 32) // Will log 0 to the console because null value behaves as 0.
-
-if(true){
-    var x = 5
-}
-console.log(x) // x is 5*/
-
-
-/* Variable Hoisting */
-
-console.log(x === undefined); // true
-var x = 3;
-
-/* Function hoisting */
-foo(); // "bar"
-
-function foo(){
-  console.log('bar');
+  function showOnePokemon(apiURL){
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(function(pokeData){
+    console.log(pokeData)
+    pokemonInfo = pokeData
+  })
 }
 
+function getPokemonImage(pokeId){
+    let apiURL = 'https://pokeapi.co/api/v2/pokemon-form/' + pokeId + '/';
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(function(pokeData){
+    console.log(pokeData)
+    pokemon_images = pokeData.sprites.front_default;
+    })
+}
 
-/* You can't declare a constant with the same name as a function or variable */
-// e.g
-function f(){
-  const f = 5;
-} // This will cause an error
-
-/* The properties of objects assigned to constants are not protected */
-const MY_OBJECT = {'key': 'value'};
-MY_OBJECT.key = 'otherValue';
-
-const MY_ARRAY = ['HTML', 'CSS'];
-MY_ARRAY.push('JAVASCRIPT');
-console.log(MY_ARRAY); // logs ['HTML, 'CSS', 'JAVASCRIPT']
-
-/* In JavaScript you don't have to specify the data type of a variable when you declare it
-    It also converts the data type as needed during script execution */
-
-var answer = 42;
-console.log(answer);
-answer = 'Thanks for everything...';
-console.log(answer);
-
-console.log('37' - 7) // 30
-console.log('37' + 7) // 377
-
-console.log(+1.1 + +1.1) // 2.2
-
-
-
-
-
+getAllPokemon()
